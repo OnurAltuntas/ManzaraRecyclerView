b84a04baaf6377ae27ef09c80b2c41dae844778f
Onur Altuntas <onuraltuntas50@gmail.com>
2019-08-09 17:10:54 +0300

    Manzara adapter is added.

---
+++ app/src/androidTest/java/com/example/manzararecyclerview/ManzaraAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.manzararecyclerview
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ManzaraAdapter:RecyclerView.Adapter<ManzaraViewHolder>(){
+    override fun onBindViewHolder(holder: ManzaraViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):ManzaraViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+}
+class ManzaraViewHolder(itemView: View?):RecyclerView.ViewHolder(itemView!!){
+
+}
